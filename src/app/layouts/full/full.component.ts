fbc5bfdf860e777ba76d1e129b34bd39a03b752a
Munarso <narsommunarso@gmail.com>
2019-11-25 07:00:50 +0700

    first commit

---
+++ src/app/layouts/full/full.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+@Component({
+    selector: 'app-full-layout',
+    templateUrl: './full.component.html',
+    styleUrls: ['./full.component.scss']
+})
+export class FullComponent implements OnInit {
+
+    color = 'reddark';
+    showSettings = false;
+    showMinisidebar = false;
+    showDarktheme = false;
+    showHorizontalNav = true;
+    showBoxedtheme = true;
+    public config: PerfectScrollbarConfigInterface = {};
+
+    constructor(public router: Router) { }
+
+    ngOnInit() {
+        if (this.router.url === '/') {
+            this.router.navigate(['/dashboard/dashboard1']);
+        }
+    }
+
+}
